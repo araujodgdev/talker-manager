0aeb7ba6dd526dca0059328ab9674624eba24a90
Douglas Araújo <devdoug45@gmail.com>
2023-08-17 10:08:33 -0300

    feat: cria o endpoint /talker, que retorna um array com as pessoas palestrantes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs').promises;
+
+const OK = 200;
+const CREATED = 201;
+const INTERNAL_SERVER_ERROR = 500;
+// const NOT_FOUND = 404;
+// const BAD_REQUEST = 400;
+// const UNPROCESSABLE_CONTENT = 422;
+// const UNAUTHORIZED = 401;
+
+const TALKER_FILE_PATH = path.resolve(__dirname, './talker.json');
 
 const app = express();
 app.use(express.json());
@@ -16,3 +28,19 @@ app.listen(PORT, () => {
 });
 
 // iniciando o projeto!!! :rocket:
+app.get("/talker", async (req, res) => {
+  try {
+    const talkerData = await fs.readFile(TALKER_FILE_PATH);
+    const parsed = JSON.parse(talkerData);
+
+    if (!talkerData) {
+      res.status(200).json([])
+    }
+
+    res.status(OK).json(parsed)
+  } catch (error) {
+    res.status(INTERNAL_SERVER_ERROR).send({
+      message: error.message
+    })
+  }
+})
